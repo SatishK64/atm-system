e3977f616bdcadacf256d2b90588f9fbc0f8d508
jayasivam <satish9666669@gmail.com>
2024-03-31 14:25:47 +0530

    started proj

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module casestudy.demo {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens casestudy.demo to javafx.fxml;
+    exports casestudy.demo;
+}
